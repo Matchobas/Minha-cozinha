ae35b9787c9861c2df807ea7ea29ab18c130e31b
Matchobas <maalexandria@hotmail.com>
2021-07-01 20:24:08 -0300

    Change: Creating routes for Storage

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ingredientRoutes from './ingredient.routes';
 import usersRoutes from './users.routes';
+import storageRoutes from './storages.routes';
 
 const routes = Router();
 
 routes.use('/ingredient', ingredientRoutes);
 routes.use('/user', usersRoutes);
+routes.use('/storage', storageRoutes);
 
 export default routes;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,8 @@
 import { Router, Request, Response } from 'express';
 
 import CreateUserService from '../services/CreateUserService';
-import AddIngredientToUserService from '../services/addIngredientToUserService';
 
 import UsersRepository from '../typeORM/repositories/usersRepository';
-import StoragesRepository from '../typeORM/repositories/storagesRepository';
-import IngredientsRepository from '../typeORM/repositories/ingredientsRepository';
 
 const usersRoutes = Router();
 
@@ -34,30 +31,6 @@ usersRoutes.get('/:username', async (request: Request, response: Response): Prom
     const getUser = await usersRepository.findByUsername(username);
 
     return response.json(getUser);
-  } catch (err) {
-    return response.json({ Error: err.message });
-  }
-});
-
-usersRoutes.post('/add-ingredient', async (request: Request, response: Response): Promise<Response> => {
-  try {
-    const { amount, userId, ingredientName } = request.body;
-
-    const storagesRepository = new StoragesRepository();
-    const ingredientsRepository = new IngredientsRepository();
-
-    const addIngredientToUserService = new AddIngredientToUserService(
-      storagesRepository,
-      ingredientsRepository
-    );
-
-    const storage = await addIngredientToUserService.execute({
-      userId,
-      ingredientName,
-      amount
-    });
-
-    return response.json(storage);
   } catch (err) {
     return response.json({ Error: err.message });
   }
